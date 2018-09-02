b23a013c5b48d6bc676aa34b0f32ddd5bb0849cb
Eval0day7 <619630959@qq.com>
2018-09-03 02:36:45 +0800

    delete

--- Backend/blog/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
-
-class BlogConfig(AppConfig):
-    name = 'blog'
-
-
-class SuitConfig(DjangoSuitConfig):
-    layout = 'horizontal'
